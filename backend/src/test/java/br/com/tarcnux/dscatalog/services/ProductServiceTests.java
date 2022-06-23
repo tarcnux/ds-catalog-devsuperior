91a69e078a7e87e1c1391a02a9f12676f220b10e
Tarcisio Nunes <github@tarcnux.com.br>
2022-06-23 01:03:04 -0300

    Mockito repository emulation for findAll, save and findById

--- backend/src/test/java/br/com/tarcnux/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/br/com/tarcnux/dscatalog/services/ProductServiceTests.java
@@ -1,19 +1,27 @@
 package br.com.tarcnux.dscatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.com.tarcnux.dscatalog.entities.Product;
 import br.com.tarcnux.dscatalog.repositories.ProductRepository;
 import br.com.tarcnux.dscatalog.services.exceptions.DatabaseException;
 import br.com.tarcnux.dscatalog.services.exceptions.ResourceNotFountException;
+import br.com.tarcnux.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -34,12 +42,23 @@ public class ProductServiceTests {
 	private long nonExistingId;
 	private long dependentId;
 	
+	private PageImpl<Product> page; //Represents a data page
+	private Product product;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		dependentId = 2L;
-		nonExistingId = 1000L;
+		nonExistingId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 		
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product); //Acho que retorna um DTO
+		
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
 		
 		// Setup the mocked behavior
 		// When call method deleteById for an existing Id, do nothing
